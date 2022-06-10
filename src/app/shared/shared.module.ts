7053f5c3fa53f5a38f4839d6c1e68d4be9f6cc7e
boysers <boyser2015@gmail.com>
2022-06-10 10:12:08 +0200

    patch: rename folder src/app/shared/  compoents => components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CommentsComponent } from './compoents/comments/comments.component';
+import { CommentsComponent } from './components/comments/comments.component';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './pipes/shorten.pipe';
